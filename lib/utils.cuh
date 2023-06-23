616842153faa0a6f3b07263d80347c4aa3275835
giuseppe-tanzi <giuseppetan@hotmail.it>
2023-06-23 17:07:09 +0200

    Improved code readability

--- lib/utils.cuh
+++ lib/utils.cuh
@@ -4,24 +4,28 @@
 #include <stdlib.h>
 #include <math.h>
 #include <cuda_runtime.h>
+#include "../lib/constants.cuh"
 
 /* Function to get the actual time*/
 double gettime(void);
 
 /* Initialize array data */
-void init_array(unsigned long *data, unsigned long nitems);
+void init_array(unsigned long *data, const unsigned long N);
 
 /* Function to print an array */
-__host__ __device__ void print_array(unsigned long *data, unsigned long size);
+__host__ __device__ void print_array(const unsigned long *data, const unsigned long N);
 
 /* Function to check if results is an ordered array */
-int check_result(unsigned long *results, unsigned long nitems);
+int check_result(unsigned long *results, const unsigned long N);
 
 /* Function to check if x is a power of 2*/
-bool IsPowerOfTwo(unsigned long x);
+bool IsPowerOfTwo(const unsigned long x);
 
 /* Function to get maximum value in data; it stores the maximum in max */
-__device__ void get_max(unsigned long *data, unsigned long n, unsigned long *max);
+__device__ void get_max(unsigned long *data, const unsigned long N, unsigned long *max);
 
 /* Function to get the power of base to exp; it stores the result in result*/
 __device__ void power(int base, int exp, unsigned *result);
+
+void determine_config(const unsigned long N, unsigned long *n_threads_per_block, unsigned long *n_blocks,
+                      unsigned long *n_total_threads, unsigned long *partition_size);
--- src/utils.cu
+++ src/utils.cu
@@ -1,7 +1,7 @@
 #include "../lib/utils.cuh"
 
-/* 
-    Function that returns the current time 
+/*
+    Function that returns the current time
 */
 double gettime(void)
 {
@@ -10,10 +10,10 @@ double gettime(void)
     return (ts.tv_sec + (double)ts.tv_nsec / 1e9);
 }
 
-/* 
-    Function that randomly initializes an array from 0 to N 
+/*
+    Function that randomly initializes an array from 0 to N
 */
-void init_array(unsigned long *data, unsigned long N)
+void init_array(unsigned long *data, const unsigned long N)
 {
     long int temp;
     srand(42); // Ensure the determinism
@@ -33,24 +33,24 @@ void init_array(unsigned long *data, unsigned long N)
     }
 }
 
-/* 
+/*
     Function that prints an array
 */
-__host__ __device__ void print_array(unsigned long *data, unsigned long size)
+__host__ __device__ void print_array(const unsigned long *data, const unsigned long N)
 {
-    for (unsigned long i = 0; i < size; i++)
+    for (unsigned long i = 0; i < N; i++)
     {
         printf("%li ", data[i]);
     }
     printf("\n");
 }
 
-/* 
-    Function that checks if the array is ordered 
+/*
+    Function that checks if the array is ordered
 */
-int check_result(unsigned long *results, unsigned long nitems)
+int check_result(unsigned long *results, const unsigned long N)
 {
-    for (unsigned long i = 0; i < nitems - 1; i++)
+    for (unsigned long i = 0; i < N - 1; i++)
     {
         if (results[i] > results[i + 1])
         {
@@ -63,7 +63,7 @@ int check_result(unsigned long *results, unsigned long nitems)
     return 1;
 }
 
-bool IsPowerOfTwo(unsigned long x)
+bool IsPowerOfTwo(const unsigned long x)
 {
     return (x & (x - 1)) == 0;
 }
@@ -71,10 +71,10 @@ bool IsPowerOfTwo(unsigned long x)
 /*
     Function that finds the maximum number in an array
 */
-__device__ void get_max(unsigned long *data, unsigned long n, unsigned long *max)
+__device__ void get_max(unsigned long *data, const unsigned long N, unsigned long *max)
 {
     *max = -INFINITY;
-    for (int i = 0; i < n; i++)
+    for (int i = 0; i < N; i++)
     {
         if (data[i] > *max)
         {
@@ -97,5 +97,90 @@ __device__ void power(int base, int exp, unsigned *result)
         if (!exp)
             break;
         base *= base;
+    }
+}
+
+void determine_config(const unsigned long N, unsigned long *n_threads_per_block, unsigned long *n_blocks,
+                      unsigned long *n_total_threads, unsigned long *partition_size)
+{
+
+    *partition_size = PARTITION_SIZE;
+
+    if (N <= *partition_size)
+    {
+        if (N <= MAXTHREADSPERBLOCK)
+        {
+            *n_blocks = 1;
+            for (unsigned long i = N; i >= 2; i--)
+            {
+                if (IsPowerOfTwo(i))
+                {
+                    *n_total_threads = i;
+                    *partition_size = ceil(N / float(*n_total_threads));
+                    *n_threads_per_block = *n_total_threads;
+                    break;
+                }
+            }
+        }
+        else
+        {
+            *n_threads_per_block = WARPSIZE;
+            *n_total_threads = WARPSIZE;
+            *n_blocks = 1;
+            *partition_size = ceil(N / (float)*n_total_threads);
+        }
+    }
+    else
+    {
+        *n_total_threads = ceil(N / (float)*partition_size);
+
+        if (*n_total_threads <= MAXTHREADSPERBLOCK)
+        {
+            *n_blocks = 1;
+            if (*n_total_threads < WARPSIZE)
+            {
+                *n_total_threads = WARPSIZE;
+                *n_threads_per_block = WARPSIZE;
+            }
+            else
+            {
+                *n_threads_per_block = *n_total_threads;
+            }
+
+            for (unsigned long i = *n_total_threads; i >= 2; i--)
+            {
+                if (IsPowerOfTwo(i))
+                {
+                    *n_total_threads = i;
+                    *partition_size = ceil(N / (float)*n_total_threads);
+                    *n_threads_per_block = *n_total_threads;
+                    break;
+                }
+            }
+        }
+        else
+        {
+            *n_threads_per_block = MAXTHREADSPERBLOCK;
+            *n_blocks = ceil(*n_total_threads / (float)*n_threads_per_block);
+
+            if (*n_blocks > MAXBLOCKS)
+            {
+                *n_blocks = MAXBLOCKS;
+            }
+
+            *n_total_threads = (unsigned long)(*n_blocks * *n_threads_per_block);
+
+            for (unsigned long i = *n_total_threads; i >= 2; i--)
+            {
+                *n_blocks = ceil(i / (float)MAXTHREADSPERBLOCK);
+                *n_total_threads = (unsigned long)(*n_blocks * *n_threads_per_block);
+
+                if (IsPowerOfTwo(*n_total_threads))
+                {
+                    *partition_size = ceil(N / (float)*n_total_threads);
+                    break;
+                }
+            }
+        }
     }
 }
