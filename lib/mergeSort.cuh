b53f48febf56a11caeaac62f232a00c09dd25f64
giuseppe-tanzi <giuseppetan@hotmail.it>
2023-07-07 12:22:48 +0200

    Moved merge kernels

--- lib/mergeSort.cuh
+++ lib/mergeSort.cuh
@@ -4,6 +4,7 @@
 #include <stdlib.h>
 #include <cuda_runtime.h>
 #include "../lib/utils.cuh"
+#include "../lib/utilsParallelSort.cuh"
 
 /* 
     Merge function of the MergeSort Algorithm 
@@ -19,3 +20,16 @@ __device__ void merge_gpu(unsigned short *data, const unsigned long long left, c
     Merge Sort Algorithm 
 */
 __host__ void merge_sort(unsigned short *data, const unsigned long long left, const unsigned long long right);
+
+/*
+    - The merge_kernel function is a CUDA kernel that performs a merging phase of the merge sort on data in parallel using total threads on one block
+    - It divides the array into smaller ranges and merges them progressively until the entire array is sorted.
+*/
+__global__ void merge_kernel(unsigned short *data, const unsigned long *offset, const unsigned long total_threads, const unsigned long total_threads_precedent_blocks);
+
+/*
+    - The merge_block_kernel is a CUDA kernel for merging sorted array on one single block.
+    - It performs a parallel merge sort on the data array using multiple threads on one single block.
+    - The function merges the array in a hierarchical manner until the entire data array is sorted.
+*/
+__global__ void merge_blocks_kernel(unsigned short *data, unsigned long long N, ParallelSortConfig config, const unsigned total_threads);
--- src/mergeSort.cu
+++ src/mergeSort.cu
@@ -141,3 +141,169 @@ __host__ void merge_sort(unsigned short *data, const unsigned long long left, co
         merge(data, left, mid, right);
     }
 }
+
+__global__ void merge_kernel(unsigned short *data, const unsigned long *offset, const unsigned long total_threads, const unsigned long total_threads_precedent_blocks)
+{
+    const unsigned long tid = total_threads_precedent_blocks + threadIdx.x;
+    unsigned long long start = 0;
+
+    unsigned long long left, mid, right, offset_merge;
+    unsigned level_merge = 0, levels_merge = 0;
+    unsigned long temp_total_threads = total_threads;
+    unsigned thread_to_merge = 0, threads_to_merge = 0;
+
+    unsigned long i;
+
+    // Compute the start for the thread, computing the offset of precedent threads
+    for (i = total_threads_precedent_blocks; i < tid; i++)
+    {
+        start += offset[i];
+    }
+
+    /*
+        - Log(total_threads)/Log(2) == Log_2(total_threads)
+        - Compute number of merge needed in the merge sort
+    */
+    while (temp_total_threads > 1)
+    {
+        temp_total_threads /= 2;
+        levels_merge++;
+    }
+
+    /*
+        - Merge the sorted array
+    */
+    for (level_merge = 0; level_merge <= levels_merge; level_merge++)
+    {
+        power(2, level_merge, &threads_to_merge);
+
+        if ((tid % threads_to_merge) == 0)
+        {
+            left = start;
+            offset_merge = offset[tid];
+
+            for (thread_to_merge = 1; thread_to_merge < threads_to_merge; thread_to_merge++)
+            {
+                offset_merge += offset[tid + thread_to_merge];
+            }
+
+            right = left + offset_merge - 1;
+
+            if (level_merge == 0)
+            {
+                mid = left + (right - left) / 2;
+            }
+
+            merge_gpu(data, left, mid, right);
+
+            /*
+                - Fix since the two merged list are of two different dimension, because the offset is balanced between threads
+                - Merge sort expects to have mid as maximum value of the first list
+            */
+            mid = right;
+        }
+        __syncthreads();
+    }
+}
+
+__global__ void merge_blocks_kernel(unsigned short *data, unsigned long long N, ParallelSortConfig config, const unsigned total_threads)
+{
+    const unsigned tid = threadIdx.x;
+    unsigned long long start = 0;
+
+    unsigned long long *block_starting_idx;
+    unsigned long long *block_size;
+    unsigned long long *thread_offset;
+    unsigned long long *thread_mid;
+    unsigned total_blocks = total_threads * 2;
+
+    unsigned long long left, mid, right, offset_merge;
+    unsigned level_merge = 0, levels_merge = 0;
+    unsigned long temp_total_threads = total_threads;
+    unsigned thread_to_merge = 0, total_threads_to_merge = 0;
+
+    unsigned i;
+
+    cudaHandleErrorGPU(cudaMalloc((void **)&block_starting_idx, total_blocks * sizeof(unsigned long long)));
+    cudaHandleErrorGPU(cudaMalloc((void **)&block_size, total_blocks * sizeof(unsigned long long)));
+    cudaHandleErrorGPU(cudaMalloc((void **)&thread_offset, total_threads * sizeof(unsigned long long)));
+    cudaHandleErrorGPU(cudaMalloc((void **)&thread_mid, total_threads * sizeof(unsigned long long)));
+
+    /*
+        - Compute the start index on the data array for each block
+        - Compute the size of the data array to handle for each block
+    */
+    get_start_index_block(block_starting_idx, N, total_blocks, config.threads_per_block, config.total_threads);
+    get_size_block(block_size, block_starting_idx, N, total_blocks, config.threads_per_block, config.total_threads);
+
+    for (unsigned block = 0; block < total_blocks; block++)
+    {
+
+        if ((block % 2) == 0)
+        {
+            // Add the offset of the successive block
+            thread_offset[block / 2] = block_size[block] + block_size[block + 1];
+
+            // Compute mid useful during the first level merge
+            thread_mid[block / 2] = 0;
+            for (i = 0; i <= block; i++)
+            {
+                thread_mid[block / 2] += block_size[i];
+            }
+        }
+    }
+
+    // Compute new start, end and offset for the thread, computing the offset of precedent threads
+    for (unsigned thread = 0; thread < tid; thread++)
+    {
+        start += thread_offset[thread];
+    }
+
+    mid = thread_mid[tid] - 1;
+
+    /*
+        - Log(n_threads)/Log(2) == Log_2(n_threads)
+        - Compute number of merge needed in the merge sort
+    */
+    while (temp_total_threads > 1)
+    {
+        temp_total_threads /= 2;
+        levels_merge++;
+    }
+
+    /*
+        - Merge the sorted array
+    */
+    for (level_merge = 0; level_merge <= levels_merge; level_merge++)
+    {
+
+        power(2, level_merge, &total_threads_to_merge);
+
+        if ((tid % total_threads_to_merge) == 0)
+        {
+            left = start;
+            offset_merge = thread_offset[tid];
+
+            for (thread_to_merge = 1; thread_to_merge < total_threads_to_merge; thread_to_merge++)
+            {
+                offset_merge += thread_offset[tid + thread_to_merge];
+            }
+
+            right = left + offset_merge - 1;
+
+            merge_gpu(data, left, mid, right);
+
+            /*
+                - Fix since the two merged list are of two different dimension, because the offset is balanced between threads.
+                - Merge sort expects to have mid as maximum value of the first list
+            */
+            mid = right;
+        }
+        __syncthreads();
+    }
+
+    cudaHandleErrorGPU(cudaFree(block_starting_idx));
+    cudaHandleErrorGPU(cudaFree(block_size));
+    cudaHandleErrorGPU(cudaFree(thread_offset));
+    cudaHandleErrorGPU(cudaFree(thread_mid));
+}
