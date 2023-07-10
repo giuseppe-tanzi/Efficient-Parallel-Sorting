b16d5683ec0c8704b0b449fb77609e9deb5b0ee1
giuseppe-tanzi <giuseppetan@hotmail.it>
2023-07-10 16:09:01 +0200

    Added shared memory

--- src/main.cu
+++ src/main.cu
@@ -12,13 +12,14 @@
 
 int main(int argc, char *argv[])
 {
-    unsigned short n_algorithms = 3;
-    char algorithms[][100] = {"Sequential Radix Sort", 
-                            "Sequential Merge Sort", 
-                            "Parallel Radix Sort + Merge Sort \\w global"};
-                            // "Parallel Radix Sort + Merge Sort \\w shared"};
-    char machine[][4] = {"CPU", "CPU", "GPU"};
+    unsigned short n_algorithms = 4;
+    char algorithms[][100] = {"Sequential Radix Sort",
+                              "Sequential Merge Sort",
+                              "Parallel Radix Sort + Merge Sort \\w global",
+                              "Parallel Radix Sort + Merge Sort \\w shared"};
+    char machine[][4] = {"CPU", "CPU", "GPU", "GPU"};
     unsigned long threads[n_algorithms];
+    bool used_shared[n_algorithms] = {false, false, false, false};
     bool correctness[n_algorithms];
     double elapsed_time[n_algorithms];
 
@@ -29,12 +30,13 @@ int main(int argc, char *argv[])
     ParallelSortConfig sort_config;
     unsigned long lists_to_merge = 0;
     unsigned blocks_involved_in_merging = 0;
+    bool shared_memory = false;
     unsigned long long *block_starting_idx;
     unsigned long long *block_size;
     unsigned long *thread_offset;
     unsigned long *dev_thread_offset;
 
-    double t_start = 0, t_stop = 0;
+    double t_start = 0, t_stop = 0, elaps_time_parallel_initialization = 0;
 
     if (argc > 1)
     {
@@ -60,7 +62,7 @@ int main(int argc, char *argv[])
     init_array(a, N);
     t_start = get_time();
     merge_sort(a, 0, N - 1);
-    t_stop= get_time();
+    t_stop = get_time();
     threads[1] = 1;
     correctness[1] = is_sorted(a, N);
     elapsed_time[1] = t_stop - t_start;
@@ -103,7 +105,21 @@ int main(int argc, char *argv[])
     thread_offset = (unsigned long *)malloc(size_blocks);
     cudaHandleError(cudaMalloc((void **)&dev_thread_offset, size_blocks));
 
-    parallel_sort(dev_a, N, sort_config, size_blocks, blocks_involved_in_merging, block_starting_idx, block_size, thread_offset, dev_thread_offset);
+    t_stop = get_time();
+
+    // Warm up
+    parallel_sort(dev_a, N, sort_config, size_blocks, blocks_involved_in_merging, block_starting_idx, block_size, thread_offset, dev_thread_offset, shared_memory);
+    cudaDeviceSynchronize();
+
+    elaps_time_parallel_initialization = t_stop - t_start;
+
+    // Global Memory
+    init_array(a, N);
+    cudaHandleError(cudaMemcpy(dev_a, a, size_array, cudaMemcpyHostToDevice));
+
+    t_start = get_time();
+
+    parallel_sort(dev_a, N, sort_config, size_blocks, blocks_involved_in_merging, block_starting_idx, block_size, thread_offset, dev_thread_offset, shared_memory);
 
     t_stop = get_time();
 
@@ -112,7 +128,27 @@ int main(int argc, char *argv[])
 
     threads[2] = sort_config.total_threads;
     correctness[2] = is_sorted(a, N);
-    elapsed_time[2] = t_stop - t_start;
+    elapsed_time[2] = t_stop - t_start + elaps_time_parallel_initialization;
+    bzero(a, size_array); // Erase destination buffer
+
+    // Shared Memory
+    init_array(a, N);
+    cudaHandleError(cudaMemcpy(dev_a, a, size_array, cudaMemcpyHostToDevice));
+
+    shared_memory = true;
+
+    t_start = get_time();
+
+    used_shared[3] = parallel_sort(dev_a, N, sort_config, size_blocks, blocks_involved_in_merging, block_starting_idx, block_size, thread_offset, dev_thread_offset, shared_memory);
+
+    t_stop = get_time();
+
+    cudaHandleError(cudaPeekAtLastError());
+    cudaHandleError(cudaMemcpy(a, dev_a, size_array, cudaMemcpyDeviceToHost));
+
+    threads[3] = sort_config.total_threads;
+    correctness[3] = is_sorted(a, N);
+    elapsed_time[3] = t_stop - t_start + elaps_time_parallel_initialization;
     bzero(a, size_array); // Erase destination buffer
 
     // printf("NUM_THREADS: %lu\n", sort_config.total_threads);
@@ -122,7 +158,7 @@ int main(int argc, char *argv[])
     // printf("PARTITION SIZE: %llu\n", sort_config.partition_size);
 
     // Print the table
-    print_table(n_algorithms, algorithms, machine, threads, correctness, elapsed_time);
+    print_table(n_algorithms, algorithms, machine, threads, used_shared, correctness, elapsed_time);
 
     // Cleanup
     free(a);
